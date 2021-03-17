ecbec3ed0ac4e2e56f5a384aee52fef0a99a5c17
Pablo <perellopablo1988@gmail.com>
2021-03-17 00:15:49 -0300

    Se modificaron los headers permitidos

--- server.js
+++ server.js
@@ -130,10 +130,18 @@ var corOptions = {
    allowedHeaders:  'Content-Type,Authorization,X-Requested-With'
 }
 
+app.use(function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   res.header('Allow', 'GET, POST, PUT, DELETE');
+   next();
+});
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 // app.use(cors());
-app.use(cors(corOptions));
+// app.use(cors(corOptions));
 
 app.listen(port, function() {
    console.log("Server corriendo en el puerto: " + port)
